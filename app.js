aaf51d8d5b014dd617c636cfd665c7cb3c8693da
amitmohanta259 <amitmohanta259@gmail.com>
2022-07-16 20:41:18 +0530

    update in folder structure

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 
 
+app.use(express.static('./public'))
+//get
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./'))
+});
+//post
+
+
+//put
+
+//patch
+
+
+
 //app.listen
 app.listen(5003, ()=>{
     console.log("Listening 5003 server");
